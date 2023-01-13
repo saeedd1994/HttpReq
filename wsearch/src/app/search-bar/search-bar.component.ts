bbb7ad9365e17fb084a9413e93eea986572de2e6
saeed <saeed.da1994@gmail.com>
2023-01-13 12:35:07 +0330

    use input event in a different way

--- wsearch/src/app/search-bar/search-bar.component.ts
+++ wsearch/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
+  term: string = '';
 
   constructor() { }
 
